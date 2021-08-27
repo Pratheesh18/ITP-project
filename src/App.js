b9dca833b5b742d76799d029d4e9366d7bb84164
pratheesh18 <pratheesh1218@gmail.com>
2021-08-27 10:11:32 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [product, setProduct] = useState({
+name:"react from FB",
+price:"10",
+productby:"FB"
+})
+const makePayment = token =>{
+const body = {
+token,
+product
 }
+const header = {
+"COntent-Type" : "application/json"
+}
+return fetch(`http://localhost:5000/payment`,{
+method:"POST",
+headers: header,
+body: JSON.stringify(body)
+}).then(response => {
+        \console.log(response)
+})
+.catch(err => console.log(err))
+}
+return (
+<div className="App">
+<header className="App-header">
+<StripeCheckout stripeKey="pk_test_51JRyZvERIx2AQewBRY5lvLWDs2IXsTWkhtpmukY7B0tHC4U1XxcZcyFZIPsAbT5HkJ4O2BwRXhnU3nsT5HH2eaDV00KhntCY2y" token={makePayment} name="Buy react"/>
+</header>
+</div>
+);
+}
+
 
 export default App;
